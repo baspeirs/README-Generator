76d5e8631401a5acc1e1d375b5f37fec8f46cd11
Bryce Speirs <baspeirs@gmail.com>
2020-05-20 21:51:43 -0400

    Created base files needed for the project and started sudo coding

---
+++ assets/questions.js
@@ -0,0 +1,2 @@
+// this file is for the questions object which we will be exporting to the program
+// dont forget to use module.exports
---
+++ script.js
@@ -0,0 +1,20 @@
+// elements of the readme page
+    // # makes a header // more pounds, smaller heading (like the h tags)
+    // * is a list item and tab in to make list inside list
+    // ```sh [content] ``` creates a.. box? lol
+    // to make a link   
+        // put your hyperlink text in []
+        // copy and paste the link in ()
+    //
+
+
+// we will need
+    // Title (#)
+    // Description (just regular text)
+    // Table of Contents (links)
+    // Installation (use the box thing)
+    // Usage (use the box thing)
+    // License ()
+    // Contributing 
+    // Tests
+    // Questions (provide links to your github username and your email)  
